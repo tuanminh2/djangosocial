d97bb618cb76c663dd009596d694dbd2d61554b5
tuanminh2 <aroi671620@gmail.com>
2022-10-27 16:43:42 +0700

    add model

--- sb/core/models.py
+++ sb/core/models.py
@@ -2,11 +2,15 @@ from django.db import models
 from django.forms import PasswordInput
 
 # Create your models here.
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Profile(models.Model):
-    user = pass
-    id_user = pass
-    bio = pass
-    profile_image = models.ImageField
-    location = pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profile_image = models.ImageField(
+        upload_to="profile_images", default="defaultimg.jpg")
+    location = models.CharField(max_length=100,  blank=True)
