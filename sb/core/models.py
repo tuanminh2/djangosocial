dc4c1484b261b734a334b509b303b9dff181ca16
tuanminh2 <aroi671620@gmail.com>
2022-10-27 09:00:14 +0700

    add media foilder

--- sb/core/models.py
+++ sb/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.forms import PasswordInput
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = pass
+    id_user = pass
+    bio = pass
+    profile_image = models.ImageField
+    location = pass
