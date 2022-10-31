07ef3c2adb0f9f63559632f874fab7e08e877c0e
TuanMinh7979 <minhtuanto179@gmail.com>
2022-10-31 08:18:18 +0700

    sign up sign in

--- sb/core/models.py
+++ sb/core/models.py
@@ -9,11 +9,11 @@ User = get_user_model()
 
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    id_user = models.IntegerField()
+    userId = models.IntegerField()
     bio = models.TextField(blank=True)
     profile_image = models.ImageField(
         upload_to="profile_images", default="defaultimg.jpg")
     location = models.CharField(max_length=100,  blank=True)
 
-    def __str__(self):
-        return self.user.username
+    # def __str__(self):
+    #     return self.user.username
--- sb/core/views.py
+++ sb/core/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from .models import Profile
 
 
 def index(request):
@@ -17,15 +18,18 @@ def signup(request):
         email = request.POST["email"]
         print(username)
         password = request.POST["password"]
-        confirmPassword = request.POST["confirmPassword"]
-        if password == confirmPassword:
+        password2 = request.POST["password2"]
+        if password == password2:
             if User.objects.filter(email=email).exists():
                 messages.info(request, 'Email taken')
                 return redirect('signup')
             else:
                 user = User.objects.create_user(
                     username=username, email=email, password=password)
-                user.save()
+                # savedUser = user.save()
+                new_profile = Profile.objects.create(
+                    user=user, userId=user.id)
+                new_profile.save()
                 return redirect("/")
         else:
             messages.info(request, "Password not matching")
@@ -34,3 +38,19 @@ def signup(request):
         print(password)
     else:
         return render(request, 'signup.html')
+
+
+def signin(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Authen failed')
+            return redirect('/signin')
+    else:
+        return render(request, 'signin.html')
