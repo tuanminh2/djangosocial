5455cd88ee379a6c594173c9786a01ca553c9ade
tuanminh2 <aroi671620@gmail.com>
2022-11-10 14:08:15 +0700

    udpated db

--- sb/core/models.py
+++ sb/core/models.py
@@ -11,6 +11,7 @@ User = get_user_model()
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     userId = models.IntegerField()
+    userName = models.CharField(max_length=100)
     bio = models.TextField(blank=True)
     profileimage = models.ImageField(
         upload_to="profile_images", default="defaultimg.jpg")
@@ -24,27 +25,29 @@ class Profile(models.Model):
 
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    auth = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     userName = models.CharField(max_length=100)
     image = models.ImageField(upload_to='post_images')
     caption = models.TextField()
-    create_at = models.DateTimeField(default=datetime.now())
+    createdAt = models.DateTimeField(default=datetime.now())
     no_of_likes = models.IntegerField(default=0)
     no_of_comments = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.userName
+        return self.user.username
 
 # MANY TO ONE > Post
+# MANY TO ONE > User
 
 
 class LikePost(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, default=None, related_name="likes")
-    userName = models.CharField(max_length=500)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, default=None, related_name="likes")
 
     def __str__(self):
-        return self.userName
+        return self.user.username
 
 # N2N Table store many to many of Profile n2n Profile
 
@@ -57,15 +60,18 @@ class FollowersCount(models.Model):
         return self.userName
 
 # MANY TO ONE > Post
+# MANY TO ONE > User
 
 
 class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, default=None, related_name="comments")
     content = models.CharField(max_length=500)
-    userName = models.CharField(max_length=500)
-    updated = models.DateTimeField(auto_now=True)
-    created = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, default=None, related_name="comments")
+    userName = models.CharField(max_length=100)
+    updatedAt = models.DateTimeField(auto_now=True)
+    createdAt = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.userName
+        return self.user.username
